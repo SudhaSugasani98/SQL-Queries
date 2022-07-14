2712d07934e2a04e910adb1bc13f753eda86130c
SudhaSugasani98 <98390153+SudhaSugasani98@users.noreply.github.com>
2022-07-15 01:40:37 +0530

    Query1

---
+++ SQLQuery1.sql
@@ -0,0 +1,14 @@
+
+
+create table tbleEmployee(EmployeeID int,Name nvarchar,Gender nvarchar,City nvarchar)
+Select * from  tbleEmployee
+insert into tbleEmployee values(1,'Sudha','Female','Hyderabad')
+insert into tbleEmployee values(2,'Neeraja','Female','Banglore')
+insert into tbleEmployee values(3,'Murali','Male','Vijayawada')
+drop table tbleEmployee
+create table tbleEmployee(EmployeeID int,Name nvarchar(25),Gender nvarchar(10),City nvarchar(20))
+create table Employee(EmployeeID int,Name nvarchar(25),Gender nvarchar(10),City nvarchar(20))
+insert into Employee values(1,'Sudha','Female','Hyderabad')
+insert into Employee values(2,'Neeraja','Female','Banglore')
+insert into Employee values(3,'Murali','Male','Vijayawada')
+select * from Employee
