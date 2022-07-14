2b4ab21f9d145769948710616f4c79ea8ee610ca
SudhaSugasani98 <98390153+SudhaSugasani98@users.noreply.github.com>
2022-07-15 01:43:47 +0530

    Laptop details related

---
+++ SQLQueryLaptopDetails.sql
@@ -0,0 +1,8 @@
+--create table LaptopDetails(
+--LapId int primary key,
+--LapName varchar(20) not null,
+--LapPrice int not null,
+--Lapprocessor varchar(10) not null)
+
+
+select * from LaptopDetails
